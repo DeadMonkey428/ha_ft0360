aa65e641081a7f8c915d689459fd3c6c708f089e
deadmonkey <tom.kruesi@gmail.com>
2026-05-31 17:10:42 +0200

    moving to hacs-folders

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -12,8 +12,8 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .api import FT0360ApiError, FT0360Client
-from .const import CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL, DOMAIN
+from .custom_components.api import FT0360ApiError, FT0360Client
+from .custom_components.const import CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
